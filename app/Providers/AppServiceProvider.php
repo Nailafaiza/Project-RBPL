e3dc7a9a791b63a121f49896f668453cb4b5345e
Agnaitaa <Agnaitaansw@gmail.com>
2026-04-23 13:19:05 +0700

    fix: paksa https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,4 +21,11 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+  public function boot(): void
+{
+    if (app()->environment('production')) {
+        URL::forceScheme('https');
+    }
+}
 }
