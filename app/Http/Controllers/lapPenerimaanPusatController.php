8178f30052ba4e3b7d83b20b97aa5347f1d2c26e
nailafaiza <nailafaizaramadhani0510@gmail.com>
2026-04-25 13:23:01 +0700

    liatin erornya dimana

--- app/Http/Controllers/lapPenerimaanPusatController.php
+++ app/Http/Controllers/lapPenerimaanPusatController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\penerimaan;
+use App\Models\LapPenerimaan;
 
 class lapPenerimaanPusatController extends Controller
 {
