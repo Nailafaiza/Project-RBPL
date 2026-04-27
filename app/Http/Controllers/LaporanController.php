33f0bac8f56f1a54f2d858874d17d88bb8a40555
Agnaitaa <Agnaitaansw@gmail.com>
2026-04-27 21:23:26 +0700

    fix laporan cetak admin

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -4,24 +4,26 @@ namespace App\Http\Controllers;
 
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class LaporanController extends Controller
 {
-public function cetakPdf(Request $request)
-{
-    
-    $bulan = $request->bulan;
+    public function cetakPDF(Request $request) 
+    {
+        $bulan = $request->bulan;
 
-    $data = DB::table('lap_penjualans')
-        ->select('nama_barang',
-            DB::raw('SUM(jumlah) as total_terjual'),
-            DB::raw('SUM(total_pendapatan) as total_pendapatan'))
-        ->whereMonth('created_at', $bulan)
-        ->groupBy('nama_barang')
-        ->get();
+        $data = DB::table('lap_penjualans') 
+            ->select(
+                'nama_barang',
+                DB::raw('SUM(jumlah) as total_terjual'),
+                DB::raw('SUM(total_pendapatan) as total_pendapatan')
+            )
+            ->whereMonth('created_at', $bulan)
+            ->groupBy('nama_barang')
+            ->get();
 
-    $pdf = Pdf::loadView('admin.laporan_pdf', compact('data', 'bulan'));
+        $pdf = Pdf::loadView('admin.laporan_pdf', compact('data', 'bulan'));
 
-    return $pdf->download('laporan_penjualan.pdf');
-}
+        return $pdf->download('laporan_penjualan.pdf');
+    }
 }
