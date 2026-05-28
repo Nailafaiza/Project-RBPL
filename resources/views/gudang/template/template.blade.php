b0b782e1998bad3f6cec225f20d5c59b10d19680
nailafaiza <nailafaizaramadhani0510@gmail.com>
2026-05-28 22:46:39 +0700

    edit template m gudang

--- resources/views/gudang/template/template.blade.php
+++ resources/views/gudang/template/template.blade.php
@@ -93,20 +93,13 @@
 
         .search-box {
           background: white;
-    padding: 10px 18px;
-    border-radius: 25px;
-    display: flex;
-    align-items: center;
-    width: 350px; 
-    height: 45px; 
-    box-shadow: 0 2px 8px rgba(0,0,0,0.1);
-}
-
-.search-box form {
-    display: flex;
-    align-items: center;
-    width: 100%;
-    height: 100%;
+          padding: 10px 18px;
+          border-radius: 25px;
+          display: flex;
+          align-items: center;
+          width: 350px; 
+          height: 45px; 
+          box-shadow: 0 2px 8px rgba(0,0,0,0.1);
 }
 
         .search-box input {
@@ -134,7 +127,7 @@
     <div class="sidebar">
         <div class="sidebar-top">
             <div class="logo">
-               <img src="/asset/logo.png" alt="Logo">
+                <img src="/asset/logo.png" alt="Logo">
             </div>
 
             <div class="menu">
@@ -163,22 +156,22 @@
     <div class="main">
 
         <div class="topbar">
-    <div class="search-box">
-        <form method="GET" action="/gudang/lap_stok">
-            <input 
-                type="text" 
-                name="search"
-                placeholder="Cari Data.........."
-                value="{{ request('search') }}"
-                onkeyup="this.form.submit()"
-            >
-        </form>
-    </div>
-</div>
+            <div class="search-box">
+               <form method="GET" action="/gudang/lap_stok">
+                    <input 
+                        type="text" 
+                        name="search"
+                        placeholder="Cari Data.........."
+                        value="{{ request('search') }}"
+                        onkeyup="this.form.submit()"
+                    >
+                </form>
+            </div>
 
             <div class="profile">
                 Manajer Gudang
             </div>
+        </div>
 
         <div class="content">
             @yield('content') 
